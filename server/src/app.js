0c146693ed6946007bec2c28616fa153d619be2e
Nikhil <nikhilkanojia693@gmail.com>
2026-07-01 10:33:49 +0530

    add limiter and few other safety middlewares

--- server/src/app.js
+++ server/src/app.js
@@ -1,22 +1,56 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import cors from "cors";
 import conf from "./config/conf.js";
+import morgan from "morgan";
 
 import productRoutes from "./routes/product.routes.js";
 
 import notFound from "./middleware/notFound.middleware.js";
 import errorHandler from "./middleware/error.middleware.js";
+import helmet from "helmet";
+import compression from "compression";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import limiter from "./middleware/rateLimiter.js";
 
 const app = express();
 
 app.use(
-    cors({
-        origin: conf.corsOrigin,
-    })
+    helmet({
+        crossOriginResourcePolicy: false,
+    }),
 );
 
-app.use(urlencoded({ extended: true }));
-app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(compression());
+
+app.use(
+    express.json({
+        limit: "10kb",
+    }),
+);
+
+app.use(
+    express.urlencoded({
+        extended: true,
+        limit: "10kb",
+    }),
+);
+
+app.use(mongoSanitize());
+
+app.use(hpp());
+
+app.use("/api", limiter);
+
+app.use(
+    cors({
+        origin: conf.corsOrigin.split(","),
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    }),
+);
 
 app.use("/api/products", productRoutes);
 
